4a6e72b2d8389f3cc2528e6058e25303b07e0440
IPYURA <111635507+IPYURA@users.noreply.github.com>
2023-10-16 23:33:21 +0900

    로딩 완성

--- loading/loading.js
+++ loading/loading.js
@@ -1,4 +1,4 @@
-const loader = $(".loader");
+const loader = $(".loadingPage");
 const html = $("html");
 
 html.css({ overflow: "hidden" }); //로딩 중 스크롤 방지
@@ -6,7 +6,6 @@ html.css({ overflow: "hidden" }); //로딩 중 스크롤 방지
 $(window).on("load", () => {
   setTimeout(() => {
     //  <-* 로딩속도 구현
-
     loader.fadeOut(100);
     html.css({ overflow: "auto" }); //스크롤 방지 해제
   }, 3000); //  <-* 로딩속도 구현
