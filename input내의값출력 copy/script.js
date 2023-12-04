cbb78d4f5908ee60ee91a61476739831dd76b515
IPYURA <111635507+IPYURA@users.noreply.github.com>
2023-12-04 09:03:44 +0900

    Add files via upload

---
+++ input내의값출력 copy/script.js
@@ -0,0 +1,12 @@
+const element = document.querySelector("#myText");
+const log = document.querySelector("#log");
+const handleChange = (e) => {
+  let value = e.target.value;
+  log.innerText = value;
+}
+
+// element.addEventListener("input", () => {
+//   let value = element.value;
+//   console.log(value);
+// })
+element.addEventListener("input",handleChange);
